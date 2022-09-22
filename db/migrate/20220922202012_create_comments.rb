70dbe8c177019e8e00fbc1d60f90aaaeb6e650b7
kdarlishta <72201300+kdarlishta@users.noreply.github.com>
2022-09-22 22:25:09 +0200

    Comment table created

---
+++ db/migrate/20220922202012_create_comments.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.text :content
+      t.integer :commentable_id
+      t.string :commentable_type
+      t.references :user_id, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_922_182_312) do
-  create_table 'posts', force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 20_220_922_202_012) do
+  create_table 'comments', force: :cascade do |t|
     t.text 'content'
+    t.integer 'commentable_id'
+    t.string 'commentable_type'
+    t.integer 'user_id_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.index ['user_id_id'], name: 'index_comments_on_user_id_id'
+  end
+
+  create_table 'posts', force: :cascade do |t|
+    t.text 'content'
+    t.integer 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_posts_on_user_id'
   end
 
   create_table 'users', force: :cascade do |t|
@@ -24,4 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 20_220_922_182_312) do
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
   end
+
+  add_foreign_key 'comments', 'user_ids'
+  add_foreign_key 'posts', 'users'
 end
