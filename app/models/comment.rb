dad57bde0a133bc06bb47c90c7da7096d2e95ed0
kdarlishta <72201300+kdarlishta@users.noreply.github.com>
2022-09-22 22:38:14 +0200

    Rubocop fixes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
-    ################################ ASSOCIATIONS ################################
-    belongs_to :user
-    belongs_to :commentable, polymorphic: true
-    has_many :comment_replies, class_name: 'Comment', foreign_key: 'parent_id', dependent: :destroy
-    belongs_to :parent, class_name: 'Comment', optional: true
-    
-    ################################## SETTINGS ##################################
+  ################################ ASSOCIATIONS ################################
+  belongs_to :user
+  belongs_to :commentable, polymorphic: true
+  has_many :comment_replies, class_name: 'Comment', foreign_key: 'parent_id', dependent: :destroy
+  belongs_to :parent, class_name: 'Comment', optional: true
 
-    ################################### SCOPES ###################################
+  ################################## SETTINGS ##################################
 
-    ################################## DELEGATES #################################
+  ################################### SCOPES ###################################
 
-    ################################# VALIDATIONS ################################
+  ################################## DELEGATES #################################
 
-    ################################## CALLBACKS #################################
+  ################################# VALIDATIONS ################################
+
+  ################################## CALLBACKS #################################
 end
